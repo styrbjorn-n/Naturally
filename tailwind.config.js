68bd76c2c54a7213757eb3e1b3ad6b556a159990
Styrbjörn Nordberg <styris.n@gmail.com>
2022-11-19 01:59:12 +0100

    styled nav and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,37 @@ module.exports = {
 ],
   theme: {
     extend: {},
+    width: {
+      "sm":"52px",
+      "md":"76px",
+      "lg":"96px",
+      "xl":"121px",
+      "xxl":"242px",
+      "3xl":"364px",
+      "4xl":"420px",
+      "5xl":"560px",
+    },
+    height: {
+      "sm":"52px",
+      "md":"76px",
+      "lg":"96px",
+      "xl":"121px",
+      "xxl":"242px",
+      "3xl":"364px",
+      "4xl":"420px",
+      "5xl":"560px",
+    },
+    gap: {
+      "sm":"52px",
+      "md":"76px",
+      "lg":"96px",
+      "xl":"121px",
+      "xxl":"242px",
+      "3xl":"364px",
+      "4xl":"420px",
+      "5xl":"560px",
+    }
+
   },
   plugins: [],
 }
